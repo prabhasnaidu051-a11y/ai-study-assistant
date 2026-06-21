638249e7188b6bbdde528483abe607111a7f9dd6
Prabhas Naidu <Prabhasnaidu051@gmail.com>
2026-06-21 12:23:40 +0530

    Fix bandit pre-commit configuration

--- .vulture_whitelist.py
+++
@@ -1,7 +0,0 @@
-# pylint: disable=invalid-name
-
-home = 1
-chat = 1
-upload_pdf = 1
-ask_document = 1
-generate_quiz = 1
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,4 @@
-from fastapi import FastAPI
-from fastapi import UploadFile
-from fastapi import File
+from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 
 from pydantic import BaseModel
@@ -18,13 +16,9 @@ app = FastAPI(
     version="1.0.0"
 )
 
-
-# -----------------------------
-# CORS
-# -----------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
